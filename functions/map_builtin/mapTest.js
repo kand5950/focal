e3929a1371f3c86637fdb9555fb54c6f47fb8530
Anit <anittankandasamy@gmail.com>
2022-09-23 01:04:18 +0000

    used push method to convert names to length and push into an array

---
+++ functions/map_builtin/mapTest.js
@@ -0,0 +1,7 @@
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+let arr = [];
+const lengthOfName = lighthouses.map((name) => arr.push(name.length));
+console.log(arr);
+
+// write code here to use map
+// It should output: [ 15, 13, 11, 16, 10, 11, 10 ]
