ab5ddf773970ee9dda3b318febb3ef6ae1442ba8
Anit <anittankandasamy@gmail.com>
2022-09-23 00:05:43 +0000

    added anonymous function replacing the call back

---
+++ functions/callbacks-intro/findWaldoAnonymously.js
@@ -0,0 +1,17 @@
+// The second argument/parameter is expected to be a (callback) function
+const findWaldo = function(names, found) {
+  names.forEach((element, index) => {
+    if (element === "Waldo") {
+      found(index);
+    }
+
+  });
+   
+
+};
+  
+
+
+console.log(findWaldo(["Alice", "Bob", "Waldo", "Winston"], function(index) {
+  console.log(`Found Waldo at index ${index}`);
+}));
